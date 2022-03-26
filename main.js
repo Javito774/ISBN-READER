eaf69bd11cdaf616306365a294aeb5bc8db76609
javierherrerosan <javierherrerosan@alumnos.upm.es>
2022-03-26 16:04:19 +0100

    Añadiendo funcion de cambiar camara.

--- main.js
+++ main.js
@@ -34,3 +34,47 @@ const detectCode =  ()=> {
     })
 }
 setInterval(detectCode, 100);
+
+var MediaStreamHelper = {
+    _streaming: false,
+    _stream: null,
+    _deviceId: null,
+    _indexSelected: 0,
+    getDevices: function() {
+        return navigator.mediaDevices.enumerateDevices();
+    },
+    requestStream: function() {
+        console.log("Solicitando nueva información del user media con la camara: "+ this._deviceId);
+        this.stopStreaming();
+        const videoSource = this._deviceId;
+        const constraints = {
+            video: {
+                deviceId: videoSource ? {exact: videoSource} : undefined
+            }
+        };
+        return navigator.mediaDevices.getUserMedia(constraints);
+    },
+    stopStreaming: function() {
+        clearInterval(intervalo);
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+    }
+};
+
+var videoElement = document.querySelector("video");
+
+function changeCamera() {
+    console.log("Cambiando camara");
+    MediaStreamHelper.stopStreaming();
+    MediaStreamHelper._indexSelected + 1 < videoDevices.length ?
+        MediaStreamHelper._indexSelected += 1:
+        MediaStreamHelper._indexSelected = 0;
+    MediaStreamHelper._deviceId = videoDevices[MediaStreamHelper._indexSelected].deviceId;
+    MediaStreamHelper.requestStream().then(function(stream){
+        MediaStreamHelper._stream = stream;
+        videoElement.srcObject = stream;
+    });
+}
